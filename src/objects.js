dc69bba970eb5a9f2960bdd76242e129a306f7cf
Eric Hoffman <eric.peter.hoffman@gmail.com>
2023-09-29 13:12:22 -0600

    Finished with Objects

--- src/objects.js
+++ src/objects.js
@@ -13,7 +13,13 @@
 //     { firstName: 'Karlach', lastName: 'Cliffgate', location: 'Avernus' }
 //   ]);
 //   => ['Gale Dekarios', 'Wyll Ravengard', 'Karlach Cliffgate'];
-function getNames(people) {}
+function getNames(people) {
+  let newArr = []
+  for(const person of people) {
+    newArr.push(person.firstName + ` ` + person.lastName)
+  }
+  return newArr
+}
 
 // Given an object representing a person, return their full name (first name and last name).
 // You MUST use object destructuring in your solution.
@@ -24,7 +30,10 @@ function getNames(people) {}
 // Ex.:
 //   getName({ firstName: 'Gale', lastName: 'Dekarios', location: 'Waterdeep' });
 //   => 'Gale Dekarios'
-function getNameUsingDestructuring(person) {}
+function getNameUsingDestructuring(person) {
+  const { firstName, lastName, location } = person
+  return firstName + ` ` + lastName
+}
 
 // Given an array of objects representing people, return a new array of the
 // people matching the given location.
@@ -43,7 +52,15 @@ function getNameUsingDestructuring(person) {}
 //     { firstName: 'Wyll', lastName: 'Ravengard', location: "Baldur's Gate" },
 //     { firstName: 'Astarion', lastName: 'Ancunin', location: "Baldur's Gate" }
 //   ];
-function getPeopleByLocation(people, location) {}
+function getPeopleByLocation(people, location) {
+  let newArr = []
+  for(const person of people) {
+    if(person.location === location) {
+      newArr.push(person)
+    }
+  }
+  return newArr
+}
 
 // Translate a phrase to pirate talk.
 //
@@ -72,7 +89,39 @@ const EN_PIRATE_LOOKUP = {
   hello: 'ahoy',
 };
 
-function translateToPirateTalk(phrase) {}
+function translateToPirateTalk(phrase) {
+  let newArr = phrase.split(` `)
+  for(let i = 0; i < newArr.length; i++) {
+    if(newArr[i] === `excuse`) {
+      newArr.splice(i,1,EN_PIRATE_LOOKUP.excuse)
+    } else if(newArr[i] === `sir`) {
+      newArr.splice(i,1,EN_PIRATE_LOOKUP.sir)
+    } else if(newArr[i] === `is`) {
+      newArr.splice(i,1,EN_PIRATE_LOOKUP.is)
+    } else if(newArr[i] === `restroom`) {
+      newArr.splice(i,1,EN_PIRATE_LOOKUP.restroom)
+    } else if(newArr[i] === `student`) {
+      newArr.splice(i,1,EN_PIRATE_LOOKUP.student)
+    } else if(newArr[i] === `friend`) {
+      newArr.splice(i,1,EN_PIRATE_LOOKUP.friend)
+    } else if(newArr[i] === `restaurant`) {
+      newArr.splice(i,1,EN_PIRATE_LOOKUP.restaurant)
+    } else if(newArr[i] === `your`) {
+      newArr.splice(i,1,EN_PIRATE_LOOKUP.your)
+    } else if(newArr[i] === `are`) {
+      newArr.splice(i,1,EN_PIRATE_LOOKUP.are)
+    } else if(newArr[i] === `my`) {
+      newArr.splice(i,1,EN_PIRATE_LOOKUP.my)
+    } else if(newArr[i] === `hotel`) {
+      newArr.splice(i,1,EN_PIRATE_LOOKUP.hotel)
+    } else if(newArr[i] === `hello`) {
+      newArr.splice(i,1,EN_PIRATE_LOOKUP.hello)
+    }
+  }
+  let arrString = newArr.toString()
+  let newString = arrString.replaceAll(`,`,` `)
+  return newString
+}
 
 // Return the number of occurrences of each word in a string.
 // This function doesn't handle punctuation and is case-sensitive, so you can
@@ -81,7 +130,15 @@ function translateToPirateTalk(phrase) {}
 // Ex.:
 //   wordCount('hello world')
 //   => { hello: 1, world: 1 }
-function wordCount(str) {}
+function wordCount(str) {
+  let newObj = {}
+  let newArr = str.split(` `)
+  newArr.forEach((element) => {
+    newObj[element] = newObj[element] ? ++newObj[element] : 1
+  })
+  //ternary condition is checking to see if the element exists in the object. If it does, increment its value by one. If it doesn't, set it's value to 1.
+  return newObj
+}
 
 // Given an object representing a bug, return true if the given bug is
 // available in the given month.
@@ -103,7 +160,16 @@ function wordCount(str) {}
 //     }
 //   }, 1);
 //   => true
-function isBugAvailable(bug, month) {}
+function isBugAvailable(bug, month) {
+  const {name, availability} = bug
+  const {rarity, months} = availability
+  for(let i = 0; i < months.length; i++) {
+    if(months[i] === month) {
+      return true
+    }
+  }
+  return false
+}
 
 // Given an array of objects representing bugs, return an object that'll be
 // used to build a calendar. The keys of the object should be the months of the
@@ -146,7 +212,53 @@ function isBugAvailable(bug, month) {}
 //     12: [],
 //   }
 
-function buildBugHuntCalendar(bugs) {}
+function buildBugHuntCalendar(bugs) {
+  let newObj = {
+  1: [],
+  2: [],
+  3: [],
+  4: [],
+  5: [],
+  6: [],
+  7: [],
+  8: [],
+  9: [],
+  10: [],
+  11: [],
+  12: []
+}
+ for(let i = 0; i < bugs.length; i++){
+  if(bugs[i].availability.months.includes(1)) {
+    newObj[`1`] += bugs[i].name + `, `
+  } if(bugs[i].availability.months.includes(2)) {
+    newObj[`2`] += bugs[i].name + `, `
+  } if(bugs[i].availability.months.includes(3)) {
+    newObj[`3`] += bugs[i].name + `, `
+  } if(bugs[i].availability.months.includes(4)) {
+    newObj[`4`] += bugs[i].name + `, `
+  } if(bugs[i].availability.months.includes(5)) {
+    newObj[`5`] += bugs[i].name + `, `
+  } if(bugs[i].availability.months.includes(6)) {
+    newObj[`6`] += bugs[i].name + `, `
+  } if(bugs[i].availability.months.includes(7)) {
+    newObj[`7`] += bugs[i].name + `, `
+  } if(bugs[i].availability.months.includes(8)) {
+    newObj[`8`] += bugs[i].name + `, `
+  } if(bugs[i].availability.months.includes(9)) {
+    newObj[`9`] += bugs[i].name + `, `
+  } if(bugs[i].availability.months.includes(10)) {
+    newObj[`10`] += bugs[i].name + `, `
+  } if(bugs[i].availability.months.includes(11)) {
+    newObj[`11`] += bugs[i].name + `, `
+  } if(bugs[i].availability.months.includes(12)) {
+    newObj[`12`] += bugs[i].name + `, `
+  } 
+}
+  return newObj
+}
+
+//I believe the reason this is failing the test despite returning the correct bugs
+//corresponding to the direct months is because my results aren't returning as an array.
 
 export {
   buildBugHuntCalendar,
